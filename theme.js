08b7ecc433cdea7f533fba92b89def332f02fea6
Kemal Akkoyun <kakkoyun@gmail.com>
2020-01-21 20:55:12 +0100

    Add basic footer

--- theme.js
+++ theme.js
@@ -1,7 +1,41 @@
+import React from 'react'
+
 export const theme = {
   // Customize your presentation theme here.
-  //
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
 };
+
+const Provider = props => (
+  <div>
+    {props.children}
+    <div
+      css={{
+        color: '#9EFEFF',
+        position: 'fixed',
+        left: 0,
+        bottom: 0,
+        margin: 16,
+      }}
+    >
+      @bwplotka
+    </div>
+    <div
+      css={{
+        color: '#9EFEFF',
+        position: 'fixed',
+        right: 0,
+        bottom: 0,
+        margin: 16,
+      }}
+    >
+      @kakkoyun
+    </div>
+  </div>
+)
+
+export default {
+  Provider,
+  // theme
+}
